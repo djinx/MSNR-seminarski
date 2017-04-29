43550f089d8408d6052022a88e6d866f2efda11e
theikeofficial <ajzenhamernikola@gmail.com>
2017-04-29 13:31:23 +0200

    Dodat skript za ciscenje nepotrebnih fajlova

---
+++ clean.bat
@@ -0,0 +1,13 @@
+del sekcije/*.aux
+del seminarski.aux
+del seminarski.log
+del seminarski.out
+del seminarski.synctex.gz
+del seminarski.toc
+del seminarski.blg
+del seminarski.bbl
+del 10_recenzije.aux
+del 10_recenzije.log
+del 10_recenzije.out
+del 10_recenzije.synctex.gz
+del 10_recenzije.toc
